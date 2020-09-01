1c496fb726562a3d7536ad5b8c231a2c6644adb9
Henrique <henriqueyano2@hotmail.com>
2020-09-01 16:06:15 -0300

    New bg an feature tests

--- core/src/com/vitinho/game/vitinho_game_main.java
+++ core/src/com/vitinho/game/vitinho_game_main.java
@@ -8,20 +8,53 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import java.awt.event.TextEvent;
 
-public class vitinho_game_main extends ApplicationAdapter {
+public class vitinho_game_main extends ApplicationAdapter
+{
 	SpriteBatch batch;
 	Texture background;
-	
+	Texture[] vitinhos;
+
+	int vitinho_state = 0;
+	int pause = 0;
+
+
 	@Override
-	public void create () {
+	public void create ()
+	{
 		batch = new SpriteBatch();
-		background = new Texture("bg.png");
+		background = new Texture("mmbg.jpg");
+		vitinhos = new Texture[4];
+		vitinhos[0] = new Texture("frame-1.png");
+		vitinhos[1] = new Texture("frame-2.png");
+		vitinhos[2] = new Texture("frame-3.png");
+		vitinhos[3] = new Texture("frame-4.png");
 	}
 
 	@Override
 	public void render () {
+
+
+
+		if(pause < 8)
+		{
+			pause++;
+		}
+		else
+		{
+			pause = 0;
+			if(vitinho_state < 3)
+			{
+				vitinho_state++;
+			}
+			else
+			{
+				vitinho_state = 0;
+			}
+		}
+
 		batch.begin();
 		batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+		batch.draw(vitinhos[vitinho_state],Gdx.graphics.getWidth()/2 - vitinhos[vitinho_state].getWidth()/2,Gdx.graphics.getHeight()/2);
 		batch.end();
 
 
