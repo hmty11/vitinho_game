6bc86f0b1207c1bbde550ace95b8a8d9361cbb88
Henrique <henriqueyano2@hotmail.com>
2020-09-01 13:27:05 -0300

    testing setup

--- core/src/com/vitinho/game/vitinho_game_main.java
+++ core/src/com/vitinho/game/vitinho_game_main.java
@@ -6,28 +6,29 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.awt.event.TextEvent;
+
 public class vitinho_game_main extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture background;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background = new Texture("bg.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 		batch.end();
+
+
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
